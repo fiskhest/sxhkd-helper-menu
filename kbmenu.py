7339a3e3f075773b3ed41018f135b99e9b03e28c
Johan Radivoi <johan@radivoj.se>
2020-06-26 17:56:17 +0200

    Validate initial use case, first PoC assured

--- kbmenu.py
+++ kbmenu.py
@@ -1,56 +1,100 @@
 #!/usr/bin/env python
 import os
 import re
+import argparse
 
-HOME=os.getenv('HOME')
+HOME = os.getenv('HOME')
 
-keybinds = list()
-config = os.getenv('shxkd_config', f'{HOME}/.config/sxhkd/sxhkdrc')
+childs_to_transform = list()
+config = os.getenv('sxhkd_config', f'{HOME}/.config/sxhkd/sxhkdrc')
 descriptor = os.getenv('descriptor', '# ')
 
-def assemble_output(data):
-    breakpoint()
-    exit()
+
+def transform_block(block):
+    indent = re.sub(r"\n[\t\s]+", "\n", block)
+    trim_trailing_commands = re.sub(r"\\\n", "", indent)
+    block_row = re.split('\\n', trim_trailing_commands, 2)
+
+    return block_row
 
 
-def parse_cfg(config, descriptor):
+def get_raw_config(config, descriptor):
+    keybinds = list()
     with open(config, "r") as cfg:
         data = cfg.read()
 
-    block_regex = r"# [\w\s\(\),\-\/&]+\n[\w\s+\d{}_\-,]+\n[\s+\t]+[\w\s\-_$'\\~%{,!.\/\(\)};\"]+\n\n"
-    eligible_blocks = re.findall(block_regex, data)
+    return data
+
+
+def parse_keybinds(config):
+    block_regex = descriptor + r"[\w\s\(\),\-\/&]+\n[\w\s+\d{}_\-,]+\n[\s+\t]+[\w\s\-_$'\\~%{,!.\/\(\)};\"\n]+\n\n"
+    eligible_blocks = re.findall(block_regex, config)
+
+    keybinds = list()
 
     for block in eligible_blocks:
-        substitute = re.sub(r"\n[\t\s]+", "\n", block)
-        block_rows = re.split('\n', substitute)
-        keybinds.append(tuple((list(filter(None, block_rows)))))
+        transformed_line = transform_block(block)
+        desc = transformed_line[0].strip(descriptor)
+        cmd = re.sub(r'\n', '', transformed_line[2])
+        unchained_lines = unchain_line(transformed_line)
+
+        for line in unchained_lines:
+            keybinds.append((desc, line, cmd))
+    
+    return keybinds
 
 
-def format_output(keybinds):
-    for desc, keybind, cmd in keybinds:
-        print(f'{desc} # {keybind} # {cmd}')
+def unchain_line(row):
+    chained_cmds = list()
+    for child in row:
+        chain = re.search(r'(?<={).*(?=})', child)
+        if chain:
+            #chain = re.sub(r'\s\+\s', '', chain.group(0))
+            return unchain(chain.group(0), child)
+            #chained_cmds.append(chain)
+            #breakpoint()
+
+    #if chained_cmds:
+    #    return unchain(chained_cmds, row)
+    #else:
+    #    return row
 
 
-def split_chains(keybinds):
-    chainbinds = list()
-    for block in keybinds:
-        check_block(block)
+
+def unchain(cmds, row):
+    rows = list()
+    for cmd in cmds.split(','):
+        rows.append(delim_row(cmd, row))
+    
+    return rows
 
 
-def check_block(block):
-    childs_to_transform = list()
-    for child in block:
-        if re.search(r'{.*}', child):
-            childs_to_transform.append(block)
-            break
+def delim_row(cmd, line):
+    """ places a delimiter (+) on the row based on chain position (start of line, in the middle or at the end) """
+    if '+' in cmd:
+        cmd = re.sub(r'\s\+', '', cmd)
 
-    breakpoint()
+    pos_in_chain = ["^{.*}", "\s{.*}\s", "{.*}$"]
+    delim_in_chain = [f"{cmd} +", f" {cmd} + ", f"{cmd}"]
+    positions = zip(pos_in_chain, delim_in_chain)
+
+    for pos, delim in positions: 
+        match = re.search(fr'{pos}', line)
+        if match:
+            if '_' in cmd:  # check for wildcard
+                return re.sub(fr'{pos}', ' ', line)
+            return re.sub(fr'{pos}', fr'{delim}', line)
 
 
 def main(config, descriptor):
-    parse_cfg(config, descriptor)
-    split_chains(keybinds)
-    format_output(keybinds)
+    raw_cfg = get_raw_config(config, descriptor)
+    binds = parse_keybinds(raw_cfg)
+    for bind in binds:
+        original_desc, keybind, original_cmd = bind
+        print(f'{original_desc}\t{keybind}\t{original_cmd}'.expandtabs(30))
+
+#        print(original_desc, bind, original_cmd)
+    #format_output(keybinds)
 
 
 if __name__ == '__main__':
